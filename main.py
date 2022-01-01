1c602c036501140113d1823e8711d6b5dbe43b1e
igodokaz <92768993+igodokaz@users.noreply.github.com>
2022-01-01 19:10:14 +0900

    2.5~6

--- main.py
+++ main.py
@@ -25,28 +25,36 @@
 # print(spans[0:-1])
 
 
-import requests
-from bs4 import BeautifulSoup
+# import requests
+# from bs4 import BeautifulSoup
 
-indeed_result = requests.get("https://www.indeed.com/jobs?q=python&limit=50")
+# indeed_result = requests.get("https://www.indeed.com/jobs?q=python&limit=50")
 
-indeed_soup = BeautifulSoup(indeed_result.text, "html.parser")
-
-pagination = indeed_soup.find("div", {"class": "pagination"})
-
-
-# for page in pages:
-#   print(page.find("span"))
-
-links = pagination.find_all('a')
-pages = []
-for link in links[:-1]:
-    # pages.append(link.find("span").string)
-    pages.append(int(link.string))
-# print(pages[-1])
-
-max_page = pages[-1]
+# indeed_soup = BeautifulSoup(indeed_result.text, "html.parser")
+# # print(indeed_soup.prettify())
+# pagination = indeed_soup.find("div", {"class": "pagination"})
 
 
+# # for page in pages:
+# #   print(page.find("span"))
 
-  
+# links = pagination.find_all('a')
+# pages = []
+# for link in links[:-1]:
+#     # pages.append(link.find("span").string)
+#     pages.append(int(link.string))
+# # print(pages[-1])
+# max_page = pages[-1]
+
+# # print(range(max_page))
+
+# for n in range(max_page):
+#     print(f"start={n*10}")
+
+
+from indeed import extract_indeed_pages, extract_indeed_jobs
+
+
+last_indeed_pages = extract_indeed_pages()
+
+indeed_jobs = extract_indeed_jobs(last_indeed_pages)
